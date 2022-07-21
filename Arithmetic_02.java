8b2d10be1b069ace93efa195a4458771ba746b12
Palak Panchal <pn_panchal28@yahoo.com>
2022-07-21 17:09:39 +0100

    added mul function in arithmetic02

--- Arithmetic_02.java
+++ Arithmetic_02.java
@@ -9,11 +9,12 @@ public class Arithmetic_02 {
         int c1 = a1+b1;
         float  d1 = b1/a1;
         int e1 = a1 - b1;
-
+        int f1 = a1 *b1;
         System.out.println("Value of a1  = "+ a1);
         System.out.println("Value of b1 = "+ b1);
         System.out.println("Sum of a1 and b1 is :"+ c1);
         System.out.println("Division of a1 and b1 is: " + d1);
         System.out.println("Subtraction of a1 and b1 = "+ e1);
+        System.out.println("Multiplication of a1 and b1 = " + f1);
     }
 }
